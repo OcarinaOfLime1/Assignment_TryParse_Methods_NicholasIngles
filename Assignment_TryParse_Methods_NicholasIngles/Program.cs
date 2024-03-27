feb4f2ae847455c91c02627f3b1914a630cb5d1c
Nicholas Ingles <nringles@student.rtc.edu>
2024-03-26 22:29:15 -0700

    Add project files.

---
+++ Assignment_TryParse_Methods_NicholasIngles/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    DownArrow();
+    UpArrow();
+    Console.WriteLine("XXXXXXXXX");
+    DownArrow();
+    UpArrow();
+
+static void DownArrow()
+{
+    Console.WriteLine("*********");
+    Console.WriteLine(" ******* ");
+    Console.WriteLine("  *****  ");
+    Console.WriteLine("   ***   ");
+    Console.WriteLine("    *    ");
+}
+static void UpArrow()
+{
+    Console.WriteLine("    *    ");
+    Console.WriteLine("   ***   ");
+    Console.WriteLine("  *****  ");
+    Console.WriteLine(" ******* ");
+    Console.WriteLine("*********");
+}
