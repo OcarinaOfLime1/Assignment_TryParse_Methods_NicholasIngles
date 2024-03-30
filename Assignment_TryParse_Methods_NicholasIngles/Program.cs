e3cc52b90290279bf25efdac65f36316f441f4e2
Nicholas Ingles <nringles@student.rtc.edu>
2024-03-29 19:28:41 -0700

    Update

--- Assignment_TryParse_Methods_NicholasIngles/Program.cs
+++ Assignment_TryParse_Methods_NicholasIngles/Program.cs
@@ -29,64 +29,74 @@ static void HourGlass()
     DownArrow();
     UpArrow();
 }
-double Additioned = 0;
-double Subtracted = 0;
-double Multiplied = 0;
-double Divided = 0;
-static void Addition(double num1, double num2)
+static void Addition(double add1, double add2)
 {
-    double Additioned = num1 * num2;
-    Console.WriteLine("wow");
-
+    double Added = add1 + add2;
+    Console.Write(Math.Round(Added,2));
 }
-static void Subtraction()
+static void Subtraction(double sub1, double sub2)
 {
-    double Subtracted = 0;
+    double Subtracted = sub1 - sub2;
+    Console.Write(Math.Round(Subtracted,2));
 }
-static void Multiplication()
+static void Multiplication(double multi1, double multi2)
 {
-
+    double Multiplied = multi1 * multi2;
+    Console.Write(Math.Round(Multiplied,2));
 }
-static void Division()
+static void Division(double divi1, double divi2)
 {
-
+    double Divided = divi1 / divi2;
+    Console.Write(Math.Round(Divided,2));
 }
-Addition(1, 2);
-Console.WriteLine(Additioned);
-Addition(3.64542, 4.812768);
-Console.WriteLine(Additioned);
 
-Console.WriteLine("\n");
-Console.WriteLine("1. 12 + 5 = ");
-Console.WriteLine("2. 902 / 72 = ");
-Console.WriteLine("3. 658 * 23 = ");
-Console.WriteLine("4. 5 - 72 = ");
-Console.WriteLine("5. 72 * 3 / 4 = ");
-Console.WriteLine("6. 17 + 42 * 76 - 19 = ");
-Console.WriteLine("7. 82.7 * (52.33 * 84) - 12 / 54 = ");
-Console.WriteLine("8. 3.14 * (5 * 5) * (5 / 3)");
+Console.Write("\n1. 12 + 5 = ");
+Addition(12, 5);
+Console.Write("\n2. 902 / 72 = ");
+Division(902, 72);
+Console.Write("\n3. 658 * 23 = ");
+Multiplication(658, 23);
+Console.Write("\n4. 5 - 72 = ");
+Subtraction(5, 72);
+Console.Write("\n5. 72 * 3 / 4 = ");
+Division(72 * 3, 4);
+Console.Write("\n6. 17 + 42 * 76 - 19 = ");
+Multiplication(17 + 42, 76 - 19);
+Console.Write("\n7. 82.7 * (52.33 * 84) - 12 / 54 = ");
+Subtraction(82.7 * (52.33 * 84), 12 / 54);
+Console.Write("\n8. 3.14 * (5 * 5) * (5 / 3) = ");
+Multiplication(14 * (5 * 5), 5 / 3);
 
 
-Part3:
-Console.Write("Input number: ");
-try
+LayoutSelect:
+Console.WriteLine("\n1. Row");
+Console.WriteLine("2. Column");
+Console.WriteLine("3. Row apart");
+Console.WriteLine("4. Column apart");
+Console.Write("Select star layout: ");
+string LayInput = Console.ReadLine();
+
+
+
+
+NumberSelect:
+int NumInput = 0;
+Console.Write("\nInput number: ");
+bool ParseInput = int.TryParse(Console.ReadLine(), out NumInput);
+
+if (ParseInput)
 {
-    int num = 2;
-    int StarNumber = int.Parse(Console.ReadLine());
-    Console.WriteLine(num);
-    Stars();
-    Console.WriteLine(num);
-    static void Stars(int num = 3)
+    Stars(NumInput);
+}
+else
+{
+    Console.WriteLine("Invalid input, try again");
+    goto NumberSelect;
+}
+static void Stars(int StarAmount)
+{
+    for (int i = 0; i < StarAmount; i++)
     {
-        Console.WriteLine(num);
-        for (int i = 0; i >= num; i++)
-        {
-        Console.WriteLine("*");
-        }
+        Console.Write("*");
     }
-}
-catch
-{
-    Console.WriteLine("Invalid input, try again.");
-    goto Part3;
 }
